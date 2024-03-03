a0d94a63d7b07a564a0a4b2205d729225a4b727c
Prathmesh <111947619+prthmh@users.noreply.github.com>
2024-03-03 11:25:11 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -7,24 +7,17 @@ import expenseRouter from "./routes/expense.routes.js";
 import savingRouter from "./routes/saving.routes.js";
 
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import helmet from "helmet";
 
 const app = express();
 
-// const allowedOrigins = ['http://localhost:5173', 'https://rupiyatrackr.netlify.app/'];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
-// app.use(cors(corsOptions));
 app.use(helmet());
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://rupiyatrackr.netlify.app/'],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/income", incomeRouter);
@@ -34,17 +27,6 @@ app.use("/saving", savingRouter);
 app.get("/", (req, res) => {
   res.send("Welcome to Finance Management API.");
 });
-
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://rupiyatrackr.netlify.app",
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
 
 app.use((req, res) => {
   res.status(404).json({ error: "Route not Found" });
